e1ad88d4b7f40d959b06d63fc43b60f6a72767a3
Ytkin Roman <mc3heisenberg@gmail.com>
2023-10-23 19:55:41 +0500

    Тест 3.
    Откат в прошлое.

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def main():
-    # Получаем запрос.
     req = request.json
-
-    # Создаём ответ.
     response = {
         "version": request.json["version"],
         "session": request.json["session"],
@@ -19,34 +16,15 @@ def main():
             "end_session": False
         }
     }
-    # Заполняем необходимую информацию.
     if req["session"]["new"]:
-        response["response"]["text"] = "Я помогу найти тебе аудиторию. Какую аудиторию ты ищешь?"
+        response["response"]["text"] = "Первое сообщение"
     else:
         if req["request"]["original_utterance"]:
-            # Инициализация.
-            c = (req["request"]["nlu"]["tokens"][0])  # Корпус.
-            au = (req["request"]["nlu"]["tokens"][1]) # Аудитория.
-            # Обрабатываем.
-            text = f"Корпус не найден...{f} {au}"
+            c = (req["request"]["nlu"]["tokens"][0])
+            text = (f"Ответ. {c}.")
             response["response"]["text"] = text
-    return json.dumps(response) # Отправляем ответ.
-    
-    
+    return json.dumps(response)
 
 
 if __name__ == "__main__":
     app.run()
-
-"""
-            try:
-                with sqlite3.connect("database.db") as db:
-                    cursor = db.cursor()
-                    query = f SELECT * FROM testing WHERE c = '{c}' AND au = '{au}' OR aua = '{au}' 
-                    cursor.execute(query)
-                    res = cursor.fetchone() # Картеж с данными из базы данных.
-                    response["response"]["text"] = res
-            except TypeError:
-                text = "Корпус не найден..."
-                response["response"]["text"] = text
-"""
