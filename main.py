2005c037cdab530077c8dfd75da4c9aab0ea1ffe
Ytkin Roman <mc3heisenberg@gmail.com>
2023-11-20 21:59:24 +0500

    35
    Интегрирование новой системы определения аудитории.

--- main.py
+++ main.py
@@ -6,6 +6,36 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
+
+def symbols_classroom(classroom):
+    characters = []
+    numbers = []
+
+    current_character = ""
+
+    for char in classroom:
+        if char.isalpha():
+            if current_character.isdigit():
+                numbers.append(current_character)
+                current_character = ""
+            current_character += char
+        elif char.isdigit():
+            if current_character.isalpha():
+                characters.append(current_character)
+                current_character = ""
+            current_character += char
+
+    # Проверка, чтобы добавить последний элемент, если он есть
+    if current_character:
+        if current_character.isdigit():
+            numbers.append(current_character)
+        else:
+            characters.append(current_character)
+
+    result = characters + numbers
+    return result
+
+
 @app.route("/webhook", methods=["POST"])
 def main():
     req = request.json
@@ -20,8 +50,11 @@ def main():
         response["response"]["text"] = "Я помогу найти тебе аудиторию. Какую аудиторию ты ищешь?"
     else:
         if req["request"]["original_utterance"]:
-            c = (req["request"]["nlu"]["tokens"][0])  # Корпус.
-            au = (req["request"]["nlu"]["tokens"][1]) # Аудитория.
+            m = req["request"]["original_utterance"].lower()
+            l = symbols_classroom(m)
+            c = l[0]  # Корпус.
+            au = l[2] # Аудитория.
+            asymb = l[1] # Символ аудитории если есть.
             try:
                 with sqlite3.connect("database.db") as db:
                     cursor = db.cursor()
